e939245c516f5072856ad793c7593c7896274eb1
jwblau2 <jeremy@abundantly.com>
2023-09-18 19:21:11 -0600

    added code to the index.js model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,20 +1,70 @@
-// import models
-const Product = require('./Product');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Import other models
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+class Product extends Model {}
 
-// Categories have many Products
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
 
-// Products belongToMany Tags (through ProductTag)
+// Define associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = Product;
