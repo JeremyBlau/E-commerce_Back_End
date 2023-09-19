91a8303b869c419d3ae9c66c4b5eb240496aa808
jwblau2 <jeremy@abundantly.com>
2023-09-18 19:26:32 -0600

    added js to the ProductTag.js model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product', // References the product model's id
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag', // References the tag model's id
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
